c87aacd2de5c1bf48a62df51b956b9003b1c158a
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-23 23:36:41 +0530

    4.Chemical Tester.cpp

---
+++ 4.Chemical Tester.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout<<"(hEm_L@b"<<endl;
+  cout<<"WELCOME TO CHEM LAB TESTING"<<endl;
+  float pH,chemical;
+  
+  cout<<"Enter the pH value  of chemical to be tested: ";
+  cin>>pH;
+  
+  if(pH>7.0)
+    cout<<"CHEMICAL IS BASIC ☺"<<endl;
+    
+         else if(pH<7.0)
+          cout<<"CHEMICAL IS ACIDIC ☺"<<endl;
+          
+             else
+              cout<<"CHEMICAL IS NEUTRAL ☺"<<endl;
+
+    cout<<"Enter the name of chemical: ";
+    cin>>chemical;
+
+}
