de2d6e61c6755bd92a462c321f9863aa01d60545
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-30 23:35:01 +0530

    3.Sum & average calculator.cpp

--- 3.Sum & average calculator.cpp
+++ 3.Sum & average calculator.cpp
@@ -8,11 +8,12 @@ int main()
 	float average;
 	cout<<"Enter number upto which the sum has to calculate:";
 	cin>>n;
-      for(i=0;i<=n;i++)
-       {
+      
+    for(i=0;i<=n;i++)
+        {
     	    cout<<"number are:"<<i<<endl;
 	    sum=sum+i;   //sum
-       }
+        }
         cout<<"sum is: "<<sum<<endl;
 	average=sum/n; // average
 	cout<<"average is: "<<average<<endl;
