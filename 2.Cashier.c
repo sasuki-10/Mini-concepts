b0600fab0b4f4985e29e1360ad32fdf910d28c90
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-12-12 23:58:13 +0530

    2.Cashier.c

--- 2.Cashier.c
+++ 2.Cashier.c
@@ -9,7 +9,7 @@ int main()
       sum=sum+n;
     } 
     while(n>0);
-     printf("Total price: %d",sum);
-     printf("\nTHANKING YOU ^_^");
+       printf("Total price: %d",sum);
+       printf("\nTHANKING YOU ^_^");
     //enter the value "0" when there is no item left in your basket...
 } 
